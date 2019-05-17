2b389fca0851b05fb0166faab80f0badf2e57f42
wtj900 <wtj900@163.com>
2019-05-17 11:32:20 +0800

    初始化

---
+++ Example/WXPickerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WXPickerView
+//
+//  Created by 王史超 on 2019/5/17.
+//  Copyright © 2019 offcn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Example/WXPickerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WXPickerView
+//
+//  Created by 王史超 on 2019/5/17.
+//  Copyright © 2019 offcn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
